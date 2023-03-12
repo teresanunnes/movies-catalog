675616a05aaf7c2c8d74694c05c3e652987893ef
teresanunnes <teresanunesteixeira.2002@gmail.com>
2023-03-12 15:54:48 -0300

    primeiro commit

---
+++ js/classes.js
@@ -0,0 +1,28 @@
+class Ator{
+    constructor(id, nome){
+        this.id=id;
+        this.nome=nome;
+    }
+}
+
+class Diretor{
+    constructor(id, nome){
+        this.id=id;
+        this.nome=nome;
+    }
+}
+
+class Filme{
+    constructor(id, titulo, ano, categoria, cartaz, direcao, atores, classificacao, avaliacao, sinopse){
+        this.id=id;
+        this.titulo=titulo;
+        this.ano=ano;
+        this.categoria=categoria;
+        this.cartaz=cartaz;
+        this.direcao=direcao;
+        this.atores=atores;
+        this.classificacao=classificacao;
+        this.avaliacao=avaliacao;
+        this.sinopse=sinopse;
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,9 @@
+let inpupBuscarFilme = document.querySelector("#input-buscar-filme");
+let btnBuscarFilme = document.querySelector("#btn-buscar-filme");
+
+btnBuscarFilme.onclick = () => {
+    if(inputBuscarFilme.value.length > 0){
+        console.log(inputBuscarFilme.value);
+    }
+    return false;
+}
